e6420b9011e14bd9965b8dc98e1c6e86312605a6
jdc9111 <52749095+jdc9111@users.noreply.github.com>
2025-08-14 17:15:33 -0400

    fixed api keys

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Endpoint to serve Firebase config
+app.get("/api/firebase-config", (req, res) => {
+  res.json({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  });
+});
+
 // Debug middleware to log requests
 app.use((req, res, next) => {
   if (req.method === "POST") {
